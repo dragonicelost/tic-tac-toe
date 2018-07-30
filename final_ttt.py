49c3096fd78d0c08ecc4ee9e80d05e10d4ca425f
dragonicelost <34805160+dragonicelost@users.noreply.github.com>
2018-07-30 22:03:41 +0530

    Update final_ttt.py

--- final_ttt.py
+++ final_ttt.py
@@ -1,4 +1,5 @@
 import random
+# this fucntion displays the board that is being filled as the game progresses
 def display_board(board):
     print('\n')
     print('\t \t \t',end='')
@@ -13,6 +14,10 @@ def display_board(board):
     print('  '+board[7]+'   |'+'  '+board[8]+'   '+'|  '+board[9])
     print('\t \t \t',end='')
     print('\n')
+    
+    
+ 
+# this function decides which player will player will make the first move and it returns the players choice either X or O(usual symbol used in tic-tac-toe)       
 def player_input():
     i=random.randint(1,2)
     if i == 1:
@@ -45,19 +50,17 @@ def player_input():
             player1='X'
             player2='O'
     return [[player1,player2],first]
+# this fucntion puts the marker (X or O) on the board
 def place_marker(board,marker,position):
     board[position]=marker
 
-    
+# this fucntion checks whether the player has won or not    
 def win_check(board,mark):
     count1=0
     count2=0
     row1=1
     for i in range(1,4):
         for j in range(0,3):
-            
-            
-            
             if board[row1+j] == mark:
                 count1 +=1
             if board[i+j*3] == mark:
@@ -74,28 +77,30 @@ def win_check(board,mark):
     if board[3]== mark and board[5]== mark and board[7] == mark:
         return True
     return False
-    
+
+
+# returns true if the space if available on that position otherwise false    
 def space_check(board,position):
     return board[position] == ' '
-    
+
+
+# returns true if the board is full    
 def full_board(board):
     return board.count(' ') == 0
+#takes in postion from the user where to palce the marker and whether the given position is invalid andspace is available or not
 def player_choice(board):
     choice=int(input('\t \t \t Enter The Choice::'))
     while True:
-        
         if choice>=1 and choice<=9:
             break
         else:
             choice=int(input('\t \t \tChoose the correct choice::'))
-            
-            
-            
     check=space_check(board,choice)
     if check == True:
         return choice
     else:
         return 0
+# to replay the game or not    
 def replay():
     print('\t \t \tWANT TO PLAY AGAIN???')
     print('\t \t \t',end='')
@@ -103,38 +108,38 @@ def replay():
     if t.lower()=='yes':
         print('\n'*100)
         the_game()
-        
     else:
         print('\t \t \tThanks for Playing')
 
-
+# the_game function 
 def the_game():
+    print('\t \t \t @@@@@WELCOME TO THE GAME@@@@')
     board=[' ']*10
     board[0]='o'
     display_board(board)
     players=[['PLAYER1',0],['PLAYER2',0]]
-    k=player_input()
+    k=player_input()                             # returns which player will chosose first
     players[0][1]=k[0][0]
     players[1][1]=k[0][1]
     first=k[1]-1
-    end_game=False
-    game_tie=False
-    winner=False
+    end_game=False # end_game to check whether game has ended or not
+    game_tie=False # checks if game has tied ot not
+    winner=False # checks if game has won by player 1 or player 2 
     while not end_game:
-        in1=0
+        in1=0 # variable to check whether the move by player is valid or not
         print('\t \t \t',end='')
         print(players[first][0],':->')
-        check=player_choice(board)
-        if check != 0:
-            place_marker(board,players[first][1],check)
-            win=win_check(board,players[first][1])
-            if win:
+        check=player_choice(board)                  # asks player for postion of marker
+        if check != 0:                                # check if postion is valid and space is available
+            place_marker(board,players[first][1],check)                 # place the marker
+            win=win_check(board,players[first][1])                          # checks if the player won 
+            if win:                # if won bt P1 or P2
                 end_game=True
                 winner=True
                 display_board(board)
                 break
             b_full=full_board(board)
-            if b_full == True:
+            if b_full == True:        # if game is tied
                 game_tie=True
                 end_game=True
                 display_board(board)
@@ -142,7 +147,7 @@ def the_game():
         else:
             print('\t \t \t',end='')
             print('INVALID CHOICE!!!,Please Enter Again')
-            in1=1
+            in1=1 
         if in1==1:
             pass
         else:
@@ -165,7 +170,7 @@ def the_game():
     replay()
   
 
+# the game starts by invoking game fucntion to start the game
 
-print('\t \t \t @@@@@WELCOME TO THE GAME@@@@')
 the_game()
         
